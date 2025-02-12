ab364a1de1a460d68d0643af96f855d20b228beb
Chubak Bidpaa <chubakbidpaa@riseup.net>
2025-02-12 12:56:47 +0330

    Init and Added: The regex flags

---
+++ cheddar.h
@@ -0,0 +1,13 @@
+#ifndef CHEDDAR_H
+#define CHEDDAR_H
+
+#define REFLAG_Default 0
+#define REFLAG_CaseInsensitive (1 << 0)
+#define REFLAG_MultiLine (1 << 1)
+#define REFLAG_DotAll (1 << 2)
+#define REFLAG_Verbose (1 << 3)
+#define REFLAG_Unicode (1 << 4)
+#define REFLAG_Global (1 << 5)
+#define REFLAG_NonGreedy (1 << 6)
+
+#endif
