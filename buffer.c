a2dbae9600220b9b5b259c5ba1207db47ff6e270
Chubak Bidpaa <chubakbidpaa@riseup.net>
2025-02-14 15:10:41 +0330

    Revised: The implementation of txt buffer, added line buffer

--- buffer.c
+++ buffer.c
@@ -11,6 +11,7 @@ extern Arena *current_arena;
 
 typedef uint32_t regex_flag_t, hash_t;
 typedef struct STRBuffer str_buffer_t;
+typedef struct LINEBuffer line_buffer_t;
 typedef struct TXTBuffer txt_buffer_t;
 typedef struct REBuffer regex_buffer_t;
 typedef struct INBuffer input_buffer_t;
@@ -45,21 +46,17 @@ struct STRBuffer {
   struct STRBuffer *next;
 };
 
+struct LINEBuffer {
+  str_buffer_t *contents;
+  size_t line_no;
+  struct LINEBuffer *next;
+  struct LINEBuffer *prev;
+};
+
 struct TXTBuffer {
-  enum RopeKind {
-    ROPE_Internal,
-    ROPE_Leaf,
-  } rope_kind;
-  size_t weight;
-
-  union {
-    struct {
-      struct TXTBuffer *left;
-      struct TXTBuffer *right;
-    } internal;
-
-    str_buffer_t *leaf;
-  };
+  line_buffer_t *lines;
+  size_t num_lines;
+  size_t line_capacity;
 };
 
 struct REBuffer {
@@ -246,141 +243,84 @@ hash_t str_buffer_hash(str_buffer_t *buffer) {
   return hash;
 }
 
-txt_buffer_t *txt_buffer_new_leaf(str_buffer_t *leaf) {
-  txt_buffer_t *node = request_memory(current_arena, sizeof(txt_buffer_t));
-  node->rope_kind = ROPE_Leaf;
-  node->leaf = leaf;
-  node->weight = leaf->length;
-
-  return node;
+line_buffer_t *line_buffer_new(str_buffer_t *contents, size_t line_no) {
+  line_buffer_t *buffer = request_memory(current_arena, sizeof(line_buffer_t));
+  buffer->contents = contents;
+  buffer->line_no = line_no;
+  buffer->next = NULL;
+  buffer->prev = NULL;
 }
 
-txt_buffer_t *txt_buffer_new_internal(txt_buffer_t *left, txt_buffer_t *right) {
-  txt_buffer_t *node = request_memory(current_arena, sizeof(txt_buffer_t));
-  node->rope_kind = ROPE_Internal;
-  node->internal.left = left;
-  node->internal.right = right;
-  node->weight =
-      (left->rope_kind == ROPE_Leaf) ? left->leaf->length : left->weight;
-
-  return node;
-}
-
-void txt_buffer_apply(const txt_buffer_t *node,
-                      void (*apply_fn)(const str_buffer_t *)) {
-  if (node == NULL)
-    return;
-
-  if (node->rope_kind == ROPE_Leaf)
-    apply_fn(node->leaf);
-  else {
-    txt_buffer_apply(node->internal.left, apply_fn);
-    txt_buffer_apply(node->internal.right, apply_fn);
-  }
-}
-
-char32_t txt_buffer_index(const txt_buffer_t *node, size_t index) {
-  if (node->rope_kind == ROPE_Leaf) {
-    if (index < node->leaf->length)
-      return node->leaf->contents[index];
-    else
-      return U'\0';
-  } else {
-    if (index < node->weight)
-      return txt_buffer_index(node->internal.left, index);
-    else
-      return txt_buffer_index(node->internal.right, index - node->weight);
+line_buffer_t *line_buffer_list_append(line_buffer_t **list,
+                                       line_buffer_t *buffer) {
+  if (list == NULL || *list == NULL) {
+    *list = buffer;
+    return *list;
   }
 
-  return U'\0';
+  line_buffer_t *head = *list;
+
+  while (head->next != NULL)
+    head = head->next;
+
+  buffer->prev = head;
+  head->next = buffer;
+
+  return head;
 }
 
-str_buffer_t *txt_buffer_extract_range(const txt_buffer_t *node, size_t start,
-                                       size_t end) {
-  if (node == NULL || start >= end)
+line_buffer_t *line_buffer_list_prepend(line_buffer_t **list,
+                                        line_buffer_t *buffer) {
+  if (list == NULL || *line == NULL) {
+    *list = buffer;
+    return *list;
+  }
+
+  line_buffer_t *tail = *list;
+
+  while (tail->prev != NULL)
+    tail = tail->prev;
+
+  buffer->next = tail;
+  tail->prev = buffer;
+
+  return tail;
+}
+
+txt_buffer_t *txt_buffer_new(size_t capacity) {
+  txt_buffer_t *buffer = request_memory(current_arena, sizeof(txt_buffer_t));
+  buffer->lines =
+      request_memory(current_arena, sizeof(line_buffer_t) * capacity);
+  buffer->capacity = capacity;
+  buffer->num_lines = 0;
+  return buffer;
+}
+
+txt_buffer_t *txt_buffer_grow_capacity(txt_buffer_t *buffer,
+                                       size_t minimum_growth) {
+  size_t growth_size = (buffer->capacity + minimum_growth) * BUFFER_GROWTH_RATE;
+  TO_NEAREST_POWER_OF_TWO(growth_size);
+
+  buffer->lines = resize_memory(&current_arena, buffer->lines, buffer->capacity,
+                                growth_size, sizeof(line_buffer_t));
+  buffer->capacity = growth_size;
+
+  return buffer;
+}
+
+txt_buffer_t *txt_buffer_insert_line(txt_buffer_t *buffer,
+                                     line_buffer_t *line) {
+  if (buffer->num_lines + 1 >= buffer->capacity)
+    txt_buffer_grow_capacity(buffer, 1);
+  buffer->lines[buffer->num_lines++] = line;
+  line_buffer_list_append(&buffer->lines, line);
+  return buffer;
+}
+
+line_buffer_t *txt_buffer_get_nth_line(txt_buffer_t *buffer, size_t index) {
+  if (index >= buffer->num_lines)
     return NULL;
-
-  static str_buffer_t *result = str_buffer_new_blank(end - start);
-
-  if (node->rope_kind == ROPE_Leaf) {
-    size_t leaf_start = start;
-    size_t leaf_end = (end < node->leaf->length) ? end : node->leaf->length;
-
-    for (size_t i = leaf_start; i < leaf_end; i++)
-      result = str_buffer_add_char(result, node->leaf->contents[i]);
-  } else {
-    if (start < node->weight) {
-      size_t left_end = (end < node->weight) ? end : node->weight;
-      txt_buffer_extract_range(node->internal.left, start, left_end);
-    }
-
-    if (end > node->weight) {
-      size_t new_start = (start > node->weight) ? start - node->weight : 0;
-      size_t new_end = end - node->weight;
-      str_buffer_extract_range(node->internal.left, new_start, new_end);
-    }
-  }
-
-  return result;
-}
-
-txtbuf_pair_t *txt_buffer_split(const txt_buffer_t *node, size_t index) {
-  if (node->rope_kind == ROPE_Leaf) {
-    txt_buffer_t *left_leaf =
-        txt_buffer_new_leaf(str_buffer_new_substring(node->leaf, 0, index));
-    txt_buffer_t *right_leaf = txt_buffer_new_leaf(str_buffer_new_substring(
-        node->leaf, index, node->leaf->length - index));
-    txtbuf_pair_t *pair = request_memory(current_arena, sizeof(txtbuf_pair_t));
-    pair->left = left_leaf;
-    pair->right = right_leaf;
-    return pair;
-  } else {
-    if (index < node->weight) {
-      txtbuf_pair_t *left_split = txt_buffer_split(node->internal.left, index);
-      txt_buffer_t *new_right =
-          txt_buffer_new_internal(left_split->right, node->internal.right);
-      txtbuf_pair_t *pair =
-          request_memory(current_arena, sizeof(txtubf_pair_t));
-      pair->left = left_split->left;
-      pair->right = new_right;
-      return pair;
-    } else {
-      txtbuf_t *right_split =
-          txt_buffer_split(node->internal.right, index - node->weight);
-      txt_buffer_t *new_left =
-          txt_buffer_new_internal(node->internal.left, right_split->left);
-      txtbuf_pair_t *pair =
-          request_memory(current_arena, sizeof(txtbuf_pair_t));
-      pair->left = new_left;
-      pair->right = right_split->right;
-      return pair;
-    }
-  }
-
-  return NULL;
-}
-
-size_t txt_buffer_total_length(const txt_buffer_t *node) {
-  if (node == NULL)
-    return 0;
-
-  if (node->rope_kind == ROPE_Leaf)
-    return node->leaf->length;
-
-  return node->weight + txt_buffer_total_length(node->internal.right);
-}
-
-txt_buffer_t *txt_buffer_concat(txt_buffer_t *left, txt_buffer_t *right) {
-  if (left == NULL)
-    return right;
-  if (right == NULL)
-    return left;
-
-  txt_buffer_t *node =
-      txt_buffer_new_internal(node->internal.left, node->internal.right);
-  node->weight = txt_buffer_total_length(left);
-
-  return node;
+  return buffer->lines[index];
 }
 
 regex_buffer_t *regex_buffer_create(str_buffer_t *patt, str_buffer_t *subst,
