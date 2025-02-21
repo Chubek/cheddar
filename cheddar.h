bc3e90f16f08bb857691249dfe356f4b41bfd43c
Chubak Bidpaa <chubakbidpaa@riseup.net>
2025-02-21 18:22:52 +0330

    Added, Initialized: The main header

--- cheddar.h
+++ cheddar.h
@@ -1,13 +1,63 @@
 #ifndef CHEDDAR_H
 #define CHEDDAR_H
 
-#define REFLAG_Default 0
-#define REFLAG_CaseInsensitive (1 << 0)
-#define REFLAG_MultiLine (1 << 1)
-#define REFLAG_DotAll (1 << 2)
-#define REFLAG_Verbose (1 << 3)
-#define REFLAG_Unicode (1 << 4)
-#define REFLAG_Global (1 << 5)
-#define REFLAG_NonGreedy (1 << 6)
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct GAPBuffer gap_buffer_t;
+typedef struct ADDRBuffer addr_buffer_t;
+typedef struct REGEXPBuffer regexp_buffer_t;
+typedef struct WINBuffer win_buffer_t;
+typedef struct TABBuffer tab_buffer_t;
+
+struct TABBuffer {
+  int tab_num;
+  win_buffer_t *in_window;
+  gap_buffer_t *txt_buffer;
+  input_buffer_t *inp_buffer;
+  output_buffer_t *outp_buffer const char32_t *title;
+  bool vertical;
+  struct TABBuffer *next;
+  struct TABBuffer *children;
+};
+
+struct WINBuffer {
+  int win_id;
+  tab_buffer_t *tabs;
+  size_t num_tabs;
+  struct WINBuffer *next;
+  struct WINBuffer *prev;
+};
+
+struct GAPBuffer {
+  char32_t *contents;
+  size_t contents_size;
+  size_t gap_start;
+  size_t gap_end;
+};
+
+struct ADDRBuffer {
+  enum ADDRKind {
+    ADDR_Abs,
+    ADDR_Rel,
+    ADDR_Range,
+    ADDR_Start,
+    ADDR_End,
+    ADDR_PrevLn,
+    ADDR_NextLn,
+  } kind;
+
+  ssize_t start;
+  ssize_t end;
+};
+
+struct REGEXPBuffer {
+  const char32_t *pattern1;
+  const char32_t *pattern2;
+  const char32_t *replace;
+  command_t *action;
+};
 
 #endif
